e067996cb7dabf4c6d78966fe87f9e2e409be036
Chaion Saha <sahachaion2000@gmail.com>
2022-09-19 04:54:44 +0600

    added new files

---
+++ 1472 A/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _1472_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop = int.Parse(Console.ReadLine());
+            for (int i = 0; i < loop; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int w = int.Parse(input[0]);
+                int h = int.Parse(input[1]);
+                int n = int.Parse(input[2]), w1=w, h1=h;
+                int count = 0;
+                bool isContinue = true;
+                while(isContinue)
+                {
+                    if(w % 2 == 0)
+                    {
+                        w = w / 2;
+                        
+                    }
+                    else if(h % 2 == 0)
+                    {
+                        h = h / 2;
+                        
+                    }
+                    else
+                    {
+                        isContinue=false;
+                    }
+                }
+                count = (w1 * h1) / (w * h);
+                //Console.WriteLine($"count={count}, n={n}");
+                if(count>=n)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ 1535 A/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _1535_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop= int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < loop; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int[] firstPair = { int.Parse(input[0]), int.Parse(input[1]) };
+                int[] secondPair = { int.Parse(input[2]), int.Parse(input[3]) };
+                int[] maxArray = { firstPair.Max(), secondPair.Max() };
+
+                int[] numbers = new int[input.Length];
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    numbers[j] = int.Parse(input[j]);
+                }
+
+                Array.Sort(numbers);
+                Array.Reverse(numbers);
+
+                Array.Sort(maxArray);
+                Array.Reverse(maxArray);
+
+                //Console.WriteLine($"{string.Join(" ", numbers)}\n{string.Join(" ", maxArray)}");
+
+                if (numbers[0] == maxArray[0] && numbers[1] == maxArray[1])
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
