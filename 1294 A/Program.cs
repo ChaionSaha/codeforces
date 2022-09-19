06fd26ecdcb106c506a2461ffb5cd014833dadc0
Chaion Saha <sahachaion2000@gmail.com>
2022-09-20 05:54:25 +0600

    added new files

--- 1294 A/Program.cs
+++ 1294 A/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _1294_A
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _1294_A
 {
     internal class Program
     {
@@ -10,10 +12,33 @@
                 string[] input = Console.ReadLine().Split();
                 int a = int.Parse(input[0]), b = int.Parse(input[1]), c = int.Parse(input[2]), n = int.Parse(input[3]);
 
-                if ((a + b + c + n) % 3 == 0)
+                if ((a < n && b < n && c < n) && (a + b + c + n) % 3 == 0)
+                {
                     Console.WriteLine("YES");
+                }
+
+                else if(a==b && b==c && (float)n%3==0)
+                {
+                    Console.WriteLine("YES");
+                }
+
                 else
-                    Console.WriteLine("NO");
+                {
+                    float avg = (a + b + c) / 3;
+                    if(avg == (float)n/3)
+                    {
+                        Console.WriteLine("YES");
+                    }
+                    else if ((avg - a) + (avg - b) + (avg - c) == n)
+                    {
+                        Console.WriteLine("YES");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                    }
+                }
+                
 
             }
         }
