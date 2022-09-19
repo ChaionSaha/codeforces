12cedf659f04968efa89b67adada617e09cdafd8
Chaion Saha <sahachaion2000@gmail.com>
2022-09-20 05:06:46 +0600

    added new files

---
+++ 1283 A/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _1283_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int h = int.Parse(input[0]);
+                int m = int.Parse(input[1]);
+
+                Console.WriteLine($"{((24 - h - 1) * 60) + (60 - m)}");
+            }
+        }
+    }
+}
---
+++ 1348 A/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _1348_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < loop; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                List<int> a = new List<int>();
+                List<int> b = new List<int>();
+                List<int> newArr = new List<int>();
+
+                double sumArrA = 0, sumArrB = 0;
+                bool isContinue = true, min = true, max = false;
+
+                for (int j = 0; j < n; j++)
+                {
+                    newArr.Add(j + 1);
+                }
+
+                while(isContinue)
+                {
+                    if(newArr.Count==0)
+                    {
+                        isContinue = false;
+                        break;
+                    }
+                    
+                    if(min)
+                    {
+                        a.Add(newArr.Min());
+                        newArr.Remove(newArr.Min());
+                        b.Add(newArr.Min());
+                        newArr.Remove(newArr.Min());
+                        min = false;
+                        max = true;
+                    }
+
+                    else if(max)
+                    {
+                        a.Add(newArr.Max());
+                        newArr.Remove(newArr.Max());
+                        b.Add(newArr.Max());
+                        newArr.Remove(newArr.Max());
+                        min = true;
+                        max = false;
+                    }
+                }
+
+                for(int j = 0; j < a.Count; j++)
+                {
+                    sumArrA += Math.Pow(2, (double)a[j]);
+                    sumArrB += Math.Pow(2, (double)b[j]);
+                }
+
+                Console.WriteLine($"{Math.Abs(sumArrA-sumArrB)}");
+            }
+        }
+    }
+}
