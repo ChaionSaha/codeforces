ff46176185f39696c4b253b2fb354aa4d9dae2a7
Chaion Saha <sahachaion2000@gmail.com>
2022-09-20 05:22:28 +0600

    new files added

---
+++ 1294 A/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _1294_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int a = int.Parse(input[0]), b = int.Parse(input[1]), c = int.Parse(input[2]), n = int.Parse(input[3]);
+
+                if ((a + b + c + n) % 3 == 0)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+
+            }
+        }
+    }
+}
