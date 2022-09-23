dab01172e52667ec3f18ba7766fb071840aa58e5
Chaion Saha <sahachaion2000@gmail.com>
2022-09-23 06:13:21 +0600

    new files added

---
+++ 1703 A/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1703_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                string input = Console.ReadLine().ToLower();
+
+                if(input.Equals("yes"))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
