a785ddc36d80e4b2a8a417d70ffd01f6b129bc1a
Chaion Saha <sahachaion2000@gmail.com>
2022-09-24 19:01:13 +0600

    new files added

---
+++ 1426 A/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _1426_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                int n = int.Parse(input[0]);
+                int x = int.Parse(input[1]);
+
+                int floorCount = 1, floor=2;
+                bool isContinue = true;
+
+                while(isContinue)
+                {
+                    if(n>0 && n<=2)
+                    {
+                        isContinue = false;
+                        floorCount = 1;
+                    }
+                    else
+                    {
+                        floorCount++;
+                        floor += x;
+                        if(n>=floor-x && n<=floor)
+                        {
+                            isContinue = false;
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+                Console.WriteLine(floorCount);
+            }
+        }
+    }
+}
