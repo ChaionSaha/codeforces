733fddf5a94bb1d6c1ddbb10220e8ed16f7a980c
Chaion Saha <sahachaion2000@gmail.com>
2022-10-10 03:19:18 +0600

    new files added

---
+++ 80 A/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _80_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]), m = int.Parse(input[1]), nextPrime = 0, count = 0;
+
+            for (int a0 = n + 1; ; a0++)
+            {
+                count = 0;
+                for (int a1 = 2; a1 < a0; a1++)
+                {
+                    if (a0 % a1 == 0)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+
+                if (count == 0)
+                {
+                    nextPrime = a0;
+                    break;
+                }
+            }
+
+            if (nextPrime == m)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
